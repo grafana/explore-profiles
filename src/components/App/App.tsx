88ee129a60dae72c457a5c307f5be10c5e7a5acd
eduardo aleixo <eh-am@users.noreply.github.com>
2023-05-16 18:31:31 +0100

    add light mode (#12)
    Closes https://github.com/grafana/pyroscope-app-plugin/issues/6
    
    Also fixes leaky css that was wrongly merged
    https://github.com/grafana/pyroscope-app-plugin/pull/11
    
    
    ![image](https://github.com/grafana/pyroscope-app-plugin/assets/6951209/4808e694-be3c-4bde-a195-1d19741d3e3f)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react';
 import { AppRootProps } from '@grafana/data';
 import { Route, Switch } from 'react-router-dom';
-import '@webapp/../sass/profile.scss';
+import '../../styles/styles.scss';
 import ContinuousSingleView from '@webapp/pages/ContinuousSingleView';
 import ContinuousDiffView from '@webapp/pages/ContinuousDiffView';
 import ContinuousComparisonView from '@webapp/pages/ContinuousComparisonView';
 import { Provider } from 'react-redux';
 import store from '@phlare/redux/store';
 import { PLUGIN_BASE_URL, ROUTES } from '../../constants';
+import { useTheme2 } from '@grafana/ui';
 
 // Prefixes the route with the base URL of the plugin
 export function prefixRoute(route: ROUTES): string {
   return `${PLUGIN_BASE_URL}/${route}`.replace(/\/{2,}/g, '/');
 }
 
+// Module augmentation so that typescript sees our 'custom' element
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'pyroscope-app': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
 export function App(props: AppRootProps) {
+  const theme = useTheme2();
   return (
     <Provider store={store}>
-      <div className="app">
+      <pyroscope-app className="app" data-theme={theme.name.toLowerCase()}>
         <div className="pyroscope-app">
           <Switch>
             <Route path={prefixRoute(ROUTES.COMPARISON_DIFF_VIEW)}>
@@ -32,7 +44,7 @@ export function App(props: AppRootProps) {
             </Route>
           </Switch>
         </div>
-      </div>
+      </pyroscope-app>
     </Provider>
   );
 }
