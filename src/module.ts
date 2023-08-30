99e8d8c64a6092b758e7de7a80cd60a453975b12
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2023-08-30 15:05:45 -0400

    fix: update link urls to use plugin prefix (#53)
    - Resolves #23

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,8 @@
 import { AppPlugin } from '@grafana/data';
+import { config } from '@grafana/runtime'
 import { App } from './components/App';
 import { AppConfig } from './components/AppConfig';
+import {PAGES} from 'grafana-pyroscope/public/app/pages/urls'
 
 export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
   title: 'Configuration',
@@ -8,3 +10,18 @@ export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
   body: AppConfig,
   id: 'configuration',
 });
+
+type PageType = keyof typeof PAGES
+
+// Note that this code will be executed once the first time the plugin has been active on screen.
+// The purpose of this code is to override any pyroscope definitions and change them to plugin app equivalents.
+
+const GRAFANA_BASE_URL = `/${config.appSubUrl}`;
+const PLUGIN_PREFIX = `${GRAFANA_BASE_URL}a/grafana-pyroscope-app`;
+
+// Change URL names where they don't match pyroscope
+PAGES.CONTINOUS_SINGLE_VIEW = '/single'; // It isn't the default `/` in this app as it is in pyroscope.
+
+// Update the URLs on PAGES to use the PLUGIN_PREFIX
+Object.keys(PAGES).forEach((page) => PAGES[page as PageType] = `${PLUGIN_PREFIX}${PAGES[page as PageType]}`)
+
