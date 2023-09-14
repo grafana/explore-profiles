0ce5d5c183096068d3b0c90d4c2c873fed2b080e
Bryan Huhta <32787160+bryanhuhta@users.noreply.github.com>
2023-09-14 16:35:10 -0500

    chore: Update standard smoke test load (#69)

---
+++ samples/rideshare/load-generator.py
@@ -0,0 +1,38 @@
+import os
+import random
+import requests
+import time
+
+VEHICLES = [
+    'bike',
+    'scooter',
+    'car',
+]
+
+DEBUG = os.environ["DEBUG"] == "1"
+
+def log(msg, fatal = False):
+    if fatal or DEBUG:
+        log(msg)
+
+if __name__ == "__main__":
+    log(f"starting load generator")
+
+    host_prefix = os.environ['HOST_PREFIX']
+    replicas = int(os.environ['REPLICAS'])
+
+    time.sleep(10)
+    while True:
+        replica = random.randint(1, replicas)
+        host = f'{host_prefix}-{replica}'
+        vehicle = VEHICLES[random.randint(0, len(VEHICLES) - 1)]
+
+        log(f"requesting {vehicle} from {host}")
+        try:
+            resp = requests.get(f'http://{host}:5000/{vehicle}')
+            resp.raise_for_status()
+            log(f"received {resp}")
+        except BaseException as e:
+            log(f"http error {e}", fatal=True)
+
+        time.sleep(random.uniform(0.2, 0.4))
---
+++ samples/rideshare/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"rideshare/bike"
+	"rideshare/car"
+	"rideshare/scooter"
+
+	"github.com/grafana/pyroscope-go"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+
+	otelpyroscope "github.com/pyroscope-io/otel-profiling-go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+func bikeRoute(w http.ResponseWriter, r *http.Request) {
+	bike.OrderBike(r.Context(), 1)
+	w.Write([]byte("<h1>Bike ordered</h1>"))
+}
+
+func scooterRoute(w http.ResponseWriter, r *http.Request) {
+	scooter.OrderScooter(r.Context(), 2)
+	w.Write([]byte("<h1>Scooter ordered</h1>"))
+}
+
+func carRoute(w http.ResponseWriter, r *http.Request) {
+	car.OrderCar(r.Context(), 3)
+	w.Write([]byte("<h1>Car ordered</h1>"))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	result := "<h1>environment vars:</h1>"
+	for _, env := range os.Environ() {
+		result += env + "<br>"
+	}
+	w.Write([]byte(result))
+}
+
+func main() {
+	serverAddress := os.Getenv("PYROSCOPE_SERVER_ADDRESS")
+	if serverAddress == "" {
+		serverAddress = "http://localhost:4040"
+	}
+	appName := os.Getenv("PYROSCOPE_APPLICATION_NAME")
+	if appName == "" {
+		appName = "ride-sharing-app"
+	}
+
+	containerID := os.Getenv("HOSTNAME")
+	if containerID == "" {
+		containerID = "unknown"
+	}
+
+	tp, err := setupTracing(appName, containerID, serverAddress)
+	if err != nil {
+		log.Fatalf("failed to set up tracing integration: %v", err)
+	}
+	defer func() {
+		_ = tp.Shutdown(context.Background())
+	}()
+
+	_, err = pyroscope.Start(pyroscope.Config{
+		ApplicationName: appName,
+		ServerAddress:   serverAddress,
+		AuthToken:       os.Getenv("PYROSCOPE_AUTH_TOKEN"),
+		Logger:          pyroscope.StandardLogger,
+		Tags:            map[string]string{"container_id": containerID},
+	})
+	if err != nil {
+		log.Fatalf("error starting pyroscope profiler: %v", err)
+	}
+
+	http.Handle("/", otelhttp.NewHandler(http.HandlerFunc(index), "IndexHandler"))
+	http.Handle("/bike", otelhttp.NewHandler(http.HandlerFunc(bikeRoute), "BikeHandler"))
+	http.Handle("/scooter", otelhttp.NewHandler(http.HandlerFunc(scooterRoute), "ScooterHandler"))
+	http.Handle("/car", otelhttp.NewHandler(http.HandlerFunc(carRoute), "CarHandler"))
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func setupTracing(appName string, containerID string, pyroscopeURL string) (tp *sdktrace.TracerProvider, err error) {
+	tp, err = tracerProviderDebug()
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the Tracer Provider and the W3C Trace Context propagator as globals.
+	// We wrap the tracer provider to also annotate goroutines with Span ID so
+	// that pprof would add corresponding labels to profiling samples.
+	otel.SetTracerProvider(otelpyroscope.NewTracerProvider(tp,
+		otelpyroscope.WithAppName(appName),
+		otelpyroscope.WithRootSpanOnly(true),
+		otelpyroscope.WithAddSpanName(true),
+		otelpyroscope.WithPyroscopeURL(pyroscopeURL),
+		otelpyroscope.WithProfileBaselineLabels(map[string]string{"container_id": containerID}),
+		otelpyroscope.WithProfileBaselineURL(true),
+		otelpyroscope.WithProfileURL(true),
+	))
+
+	// Register the trace context and baggage propagators so data is propagated across services/processes.
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	))
+
+	return tp, err
+}
+
+func tracerProviderDebug() (*sdktrace.TracerProvider, error) {
+	var exporter sdktrace.SpanExporter
+	if os.Getenv("DEBUG") == "1" {
+		var err error
+		exporter, err = stdouttrace.New(stdouttrace.WithPrettyPrint())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return sdktrace.NewTracerProvider(sdktrace.WithSpanProcessor(sdktrace.NewSimpleSpanProcessor(exporter))), nil
+}
