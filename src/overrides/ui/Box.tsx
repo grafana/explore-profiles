469ea4c7c8208a63c13a69843af320b6960f624a
Darren Janeczek <38694490+darrenjaneczek@users.noreply.github.com>
2023-09-21 21:15:27 -0400

    fix: overflow was hidden for diff selector in CollapseBox (#75)
    We are (temporarily) sacrificing the "collapse" capability in order to
    prevent buttons from a dropdown selector from being hidden.
    
    Problem:
    
    ![image](https://github.com/grafana/pyroscope-app-plugin/assets/38694490/2909dae0-6254-4240-83c0-7416bcfe0fa3)
    
    Fix:
    
    ![image](https://github.com/grafana/pyroscope-app-plugin/assets/38694490/aff3b797-51bd-4da0-8750-cf17ec803d23)

--- src/overrides/ui/Box.tsx
+++ src/overrides/ui/Box.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Collapse, PanelContainer, useStyles2 } from '@grafana/ui';
+import { PanelContainer, useStyles2 } from '@grafana/ui';
 import { css } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
 
@@ -17,18 +17,17 @@ function Box(props: BoxProps) {
 export function CollapseBox({ title, children, isLoading }: CollapseBoxProps) {
   const [open, setOpen] = useState(true);
 
-  return (
-    <Collapse label={title} collapsible isOpen={open} onToggle={() => setOpen(!open)} loading={isLoading}>
-      {children}
-    </Collapse>
-  );
+  const s = useStyles2(getStyles);
+
+  return <PanelContainer title={title} className={s.box}>
+    {children}
+  </PanelContainer>
 }
 
 const getStyles = (theme: GrafanaTheme2) => ({
-  collapseAdjustments: css``,
   box: css`
     margin-bottom: ${theme.spacing(1)};
-    padding-top: ${theme.spacing(1)};
+    padding: ${theme.spacing(1)};
     position: relative;
   `,
 });
