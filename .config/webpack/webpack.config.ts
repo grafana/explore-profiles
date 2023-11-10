a2d662089197c0ca10f805330147457b6ecdbe51
Dmitry Filimonov <dmitry.filimonov@grafana.com>
2023-11-10 13:50:49 -0600

    chore: removes LICENSE file (#183)
    See [this
    convo](https://raintank-corp.slack.com/archives/C048VSWPZ4P/p1699633535366069?thread_ts=1699626534.565829&cid=C048VSWPZ4P)

--- .config/webpack/webpack.config.ts
+++ .config/webpack/webpack.config.ts
@@ -144,7 +144,6 @@ const config = async (env): Promise<Configuration> => ({
         // To `compiler.options.output`
         { from: hasReadme() ? 'README.md' : '../README.md', to: '.', force: true },
         { from: 'plugin.json', to: '.' },
-        { from: '../LICENSE', to: '.' },
         { from: '../CHANGELOG.md', to: '.', force: true },
         { from: '**/*.json', to: '.' }, // TODO<Add an error for checking the basic structure of the repo>
         { from: '**/*.svg', to: '.', noErrorOnMissing: true }, // Optional
