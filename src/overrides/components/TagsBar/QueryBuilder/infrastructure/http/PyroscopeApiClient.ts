33376800a15043c540451ff09eec98aed90a6688
Marc M <146180665+grafakus@users.noreply.github.com>
2023-11-28 20:51:58 +0100

    fix(QueryBuilder): Fix PyroscopeApiClient base URL (#217)

--- src/overrides/components/TagsBar/QueryBuilder/infrastructure/http/PyroscopeApiClient.ts
+++ src/overrides/components/TagsBar/QueryBuilder/infrastructure/http/PyroscopeApiClient.ts
@@ -1,8 +1,12 @@
 import { tenantIDFromStorage } from '@pyroscope/services/storage';
 import { HttpClient } from './HttpClient';
+import { config } from '@grafana/runtime';
 
 export class PyroscopeApiClient extends HttpClient {
-  static apiBaseUrl = '/api/plugins/grafana-pyroscope-app/resources/querier.v1.QuerierService';
+  static apiBaseUrl = new URL(
+    '/api/plugins/grafana-pyroscope-app/resources/querier.v1.QuerierService',
+    config.appUrl
+  ).toString();
 
   constructor() {
     super(PyroscopeApiClient.apiBaseUrl, {
