2e66a612e38ea979751f186f97ccdaffaccfc2cc
Ryan Perry <Rperry2174@gmail.com>
2023-12-06 01:22:28 -0500

    tweak prompt for better formatting in responses (#234)
    ### ✨ Description
    
    **Related issue(s):**
    
    <!-- General summary of what the PR aims to do -->
    
    ### 📖 Summary of the changes
    
    <!-- Summary of the most important changes, the choices made (and why)
    and any other relevant info that will help your reviewers -->
    
    ### 🧪 How to test?
    
    <!-- Steps required to test the PR or pointer to the automated tests -->
    <!-- For UI changes, don't hesitate to provide before/after screenshots
    -->

--- src/pages/ContinuousSingleViewAi/components/ai/hooks/buildLlmPrompts.tsx
+++ src/pages/ContinuousSingleViewAi/components/ai/hooks/buildLlmPrompts.tsx
@@ -19,30 +19,22 @@ const prompts: Record<PromptCategories, Prompts> = {
   user: {
     // add new user prompts above
     ryan: (profile: string, profileType: string) => `
-interpret this flamegraph which is provided in dot format for me and answer the following three questions:
-- Performance Bottleneck: What's slowing things down (or consuming too much memory)?
-- Root Cause: Why is this happening?
-- Recommended Fix: How can we resolve it?
-
-Do not break function names.
-Do not show any numeric values, absolute or percents.
-Do not show node names like N1, N3, or Node 1, Node 2.
-Do not suggest low-level runtime optimisations, focus on the user code.
-
-Always use full function names.
-Never split function and package name.
-
-Remove any numeric values, absolute or percents, from the output.
-Remove node names like N1, N3, or Node 1, Node 2 from the output.
-
-If the function is widely known (e.g., a runtime or stdlib function), provide me concise explanation why the function is present in the profile and what could be the cause.
-If a function is defined in the runtime or in the standard library, tell me which function in the user code calls it.
-Avoid mentioning functions from the same call-chain.
-
-Format the response in sections according to the three questions
-The profile type is ${profileType}
-Below is the profile in DOT format:
-${profile}
+    Analyze this flamegraph in DOT format and address these key aspects:
+    - **Performance Bottleneck**: Identify the primary factors slowing down the process, consuming excessive memory, or causing a bottlneck in the system.
+    - **Root Cause**: Explain clearly why these bottlenecks are occurring.
+    - **Recommended Fix**: Suggest practical solutions for these issues.
+    
+    Guidelines:
+    - Always use full function names without splitting them from package names.
+    - Exclude numeric values, percentages, and node names (e.g., N1, N3, Node 1, Node 2).
+    - Focus on user code over low-level runtime optimizations.
+    - For standard library or runtime functions, explain their presence/function and link them to user code functions calling them. Avoid repetitive mentions from the same call chain.
+    
+    Format the response using markdown headers for each section corresponding to the key aspects.
+    
+    The profile type is: ${profileType}
+    Flamegraph in DOT format:
+    ${profile}
 `,
     anton: (profile: string, profileType: string) => `
 Give me actionable feedback and suggestions on how I improve the application performance.
