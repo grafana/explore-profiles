c141e37d66dd3308166c387a77c33c73bce88eee
Bryan Huhta <32787160+bryanhuhta@users.noreply.github.com>
2024-01-25 10:38:42 -0600

    feat: Change output of mage build (#270)

--- Magefile.go
+++ Magefile.go
@@ -4,9 +4,26 @@
 package main
 
 import (
-	// mage:import
+	"fmt"
+	"os"
+
+	//mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
 // Default configures the default target.
 var Default = build.BuildAll
+
+func init() {
+	err := build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
+		outputPath := os.Getenv("MAGE_OUTPUT_FOLDER")
+		if outputPath != "" {
+			cfg.OutputBinaryPath = outputPath
+		}
+
+		return cfg, nil
+	})
+	if err != nil {
+		panic(fmt.Sprintf("failed to set BeforeBuildCallback: %v", err))
+	}
+}
