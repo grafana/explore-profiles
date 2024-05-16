8eb1320e1626e925ef0ef6e154c37586d699d35a
Marc M <146180665+grafakus@users.noreply.github.com>
2024-05-16 16:36:22 +0200

    feat(AiButton): Make it more prominent (#432)

--- src/shared/components/AiPanel/components/AIButton.tsx
+++ src/shared/components/AiPanel/components/AIButton.tsx
@@ -1,5 +1,6 @@
+import { css, cx } from '@emotion/css';
 import { IconName } from '@grafana/data';
-import { Button } from '@grafana/ui';
+import { Button, useStyles2 } from '@grafana/ui';
 import { reportInteraction } from '@shared/domain/reportInteraction';
 import React, { ReactNode } from 'react';
 
@@ -14,9 +15,11 @@ type AIButtonProps = {
 };
 
 export function AIButton({ children, className, onClick, disabled, interactionName }: AIButtonProps) {
+  const styles = useStyles2(getStyles);
   const { isEnabled, error, isFetching } = useFetchLlmPluginStatus();
 
-  let icon: IconName = 'ai';
+  // let icon: IconName = 'ai'; // TODO: uncomment when the feature is more mature
+  let icon: IconName = 'fire';
 
   if (error) {
     icon = 'shield-exclamation';
@@ -31,14 +34,25 @@ export function AIButton({ children, className, onClick, disabled, interactionNa
 
   return (
     <Button
-      className={className}
+      className={cx(className, styles.aiButton)}
       onClick={onClickInternal}
       disabled={!isEnabled || disabled}
       title={isEnabled ? 'Ask FlameGrot AI' : 'Grafana LLM plugin missing or not configured!'}
       icon={icon}
-      fill="text"
+      // TODO: uncomment when the feature is more mature
+      // fill="text"
     >
       {children}
     </Button>
   );
 }
+
+const getStyles = () => ({
+  // TODO: remove when the feature is more mature
+  aiButton: css`
+    background-color: #8025ff;
+    &:not([disabled]):hover {
+      background-color: #6c27d3;
+    }
+  `,
+});
