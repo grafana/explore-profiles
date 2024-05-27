85f5c350ba0c7d5aae434927e15718400e7b80ab
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-05-27 16:20:31 +0200

    fix(*): Do not init scenes in module top level scope

--- src/pages/ProfilesExplorerView/ProfilesExplorerView.tsx
+++ src/pages/ProfilesExplorerView/ProfilesExplorerView.tsx
@@ -1,11 +1,11 @@
 import { PageTitle } from '@shared/ui/PageTitle';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { SceneProfilesExplorer } from './SceneProfilesExplorer';
 
-const sceneProfilesExplorer = new SceneProfilesExplorer();
-
 export function ProfilesExplorerView() {
+  const sceneProfilesExplorer = useMemo(() => new SceneProfilesExplorer(), []);
+
   return (
     <>
       <PageTitle title="Profiles explorer" />
