c4877394558065e5eb164735c2eaf4239a033d3e
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-06-27 15:31:19 +0200

    fix(Actions): More fixes

--- src/pages/ProfilesExplorerView/data/helpers/interpolateQueryRunnerVariables.ts
+++ src/pages/ProfilesExplorerView/data/helpers/interpolateQueryRunnerVariables.ts
@@ -1,6 +1,6 @@
 import { AdHocVariableFilter } from '@grafana/data';
 import { sceneGraph, SceneObject } from '@grafana/scenes';
-import { clone, uniqBy } from 'lodash';
+import { clone, defaults, uniqBy } from 'lodash';
 
 import { GridItemData } from '../../components/SceneByVariableRepeaterGrid/GridItemData';
 import { FiltersVariable } from '../../variables/FiltersVariable/FiltersVariable';
@@ -17,10 +17,10 @@ export function interpolateQueryRunnerVariables(
 ): InterpolatedQueryRunnerParams {
   const { queryRunnerParams } = item;
 
-  const interpolatedParams = clone(queryRunnerParams);
-
-  interpolatedParams.serviceName = sceneGraph.lookupVariable('serviceName', sceneObject)?.getValue() as string;
-  interpolatedParams.profileMetricId = sceneGraph.lookupVariable('profileMetricId', sceneObject)?.getValue() as string;
+  const interpolatedParams = defaults(clone(queryRunnerParams), {
+    serviceName: sceneGraph.lookupVariable('serviceName', sceneObject)?.getValue() as string,
+    profileMetricId: sceneGraph.lookupVariable('profileMetricId', sceneObject)?.getValue() as string,
+  });
 
   // state.filters has the AdHocFilterWithLabels[] type so we get rid of keyLabel and valueLabel
   const parsedFilters = (sceneGraph.lookupVariable('filters', sceneObject) as FiltersVariable).state.filters.map(
