b9b747fba3b1c2061ffb1f626ba527808c39a077
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-07-11 12:24:55 +0200

    chore(*): Small refactorings

--- src/pages/ProfilesExplorerView/data/series/http/formatSeriesResponse.ts
+++ src/pages/ProfilesExplorerView/data/series/http/formatSeriesResponse.ts
@@ -1,8 +1,28 @@
-import { getProfileMetric } from '@shared/infrastructure/profile-metrics/getProfileMetric';
+import { getProfileMetric, ProfileMetricId } from '@shared/infrastructure/profile-metrics/getProfileMetric';
 
 import { ServiceToProfileMetricsMap } from './SeriesApiClient';
 
-// eslint-disable-next-line sonarjs/cognitive-complexity
+function findServiceNameAndProfileMetricId(labels: Array<{ name: string; value: string }>) {
+  let serviceName;
+  let profileMetricId;
+
+  for (const { name, value } of labels) {
+    if (name === 'service_name') {
+      serviceName = value;
+    }
+
+    if (name === '__profile_type__') {
+      profileMetricId = value;
+    }
+
+    if (serviceName && profileMetricId) {
+      return [serviceName, profileMetricId];
+    }
+  }
+
+  return [];
+}
+
 export function formatSeriesResponse(data: any): ServiceToProfileMetricsMap {
   const services: ServiceToProfileMetricsMap = new Map();
 
@@ -12,33 +32,19 @@ export function formatSeriesResponse(data: any): ServiceToProfileMetricsMap {
   }
 
   for (const { labels } of data.labelsSet) {
-    let serviceName;
-    let profileMetricId;
-
-    for (const { name, value } of labels) {
-      if (name === 'service_name') {
-        serviceName = value;
-      }
-
-      if (name === '__profile_type__') {
-        profileMetricId = value;
-      }
-
-      if (serviceName && profileMetricId) {
-        break;
-      }
-    }
+    const [serviceName, profileMetricId] = findServiceNameAndProfileMetricId(labels);
 
     if (!serviceName || !profileMetricId) {
       console.warn(
-        'Pyroscope ServicesApiClient: "service_name" and/or "__profile_type__" are missing in the data received!'
+        'Pyroscope ServicesApiClient: "service_name" and/or "__profile_type__" are missing in the labels received!',
+        labels
       );
-      return services;
+      continue;
     }
 
     const serviceProfileMetrics = services.get(serviceName) || new Map();
 
-    serviceProfileMetrics.set(profileMetricId, getProfileMetric(profileMetricId));
+    serviceProfileMetrics.set(profileMetricId, getProfileMetric(profileMetricId as ProfileMetricId));
 
     services.set(serviceName, serviceProfileMetrics);
   }
