74e398b19873d8d7ff36f5ab739d6b579c36292e
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-07-11 13:38:38 +0200

    Remove commitlint :)

--- commitlint.config.ts
+++
@@ -1,8 +0,0 @@
-import { RuleConfigSeverity } from '@commitlint/types';
-
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [RuleConfigSeverity.Error, 'never', ['lower-case', 'camel-case', 'pascal-case']],
-  },
-};
