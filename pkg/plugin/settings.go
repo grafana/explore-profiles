d5a59682daaef752edf283b6d6a1cbe0bfedb3c5
Marc M <146180665+grafakus@users.noreply.github.com>
2024-07-15 06:26:02 +0200

    chore(Plugin): Remove Go backend (#21)

--- pkg/plugin/settings.go
+++
@@ -1,7 +0,0 @@
-package plugin
-
-type Settings struct {
-	BackendURL        string `json:"backendUrl"`
-	BasicAuthUser     string `json:"basicAuthUser"` // must be treated as a string
-	BasicAuthPassword string `json:"basicAuthPassword"`
-}
