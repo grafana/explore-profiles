55f420a532ee8f2d6d955112d2dd4665df18cf67
Marc M. <146180665+grafakus@users.noreply.github.com>
2024-07-18 15:22:58 +0200

    fix(Header): Switch the exploration type radio button group to a select on narrow screens (#70)

--- src/pages/ProfilesExplorerView/ProfilesExplorerView.tsx
+++ src/pages/ProfilesExplorerView/ProfilesExplorerView.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 
-import { SceneProfilesExplorer } from './SceneProfilesExplorer';
+import { SceneProfilesExplorer } from './components/SceneProfilesExplorer/SceneProfilesExplorer';
 
 export function ProfilesExplorerView() {
   const sceneProfilesExplorer = useMemo(() => new SceneProfilesExplorer(), []);
