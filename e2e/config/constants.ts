da597ad78b2650683305d6abfc5df5dfa0cf1f83
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-08-29 01:00:16 +0200

    test: Fix E2E

--- e2e/config/constants.ts
+++ e2e/config/constants.ts
@@ -22,15 +22,20 @@ function getEnvVars(): EnvVars {
 
 export const ENV_VARS = getEnvVars();
 
-export const DEFAULT_URL_PARAMS = ENV_VARS.E2E_BASE_URL.startsWith('http://localhost')
-  ? new URLSearchParams({
-      // We use static data in local and PR build (where the host is http://localhost):
-      query: 'process_cpu:cpu:nanoseconds:cpu:nanoseconds{service_name="ride-sharing-app"}',
-      from: '1710352800', // 2024-03-13 19:00:00
-      to: '1710355800', // 2024-03-13 19:50:00
-      maxNodes: '16384',
-    })
-  : new URLSearchParams();
+export const AUTH_FILE = path.join(process.cwd(), 'e2e', 'auth', 'user.json');
+
+/* Explore Profiles */
+
+export enum ExplorationType {
+  AllServices = 'all',
+  ProfileTypes = 'profiles',
+  Labels = 'labels',
+  FlameGraph = 'flame-graph',
+  DiffFlameGraph = 'diff-flame-graph',
+  Favorites = 'favorites',
+}
+
+export const DEFAULT_EXPLORE_PROFILES_DATASOURCE_UID = 'grafanacloud-profiles-local-a';
 
 export const DEFAULT_EXPLORE_PROFILES_URL_PARAMS = ENV_VARS.E2E_BASE_URL.startsWith('http://localhost')
   ? new URLSearchParams({
@@ -40,8 +45,18 @@ export const DEFAULT_EXPLORE_PROFILES_URL_PARAMS = ENV_VARS.E2E_BASE_URL.startsW
       'var-dataSource': 'grafanacloud-profiles-local-a',
       'var-serviceName': 'ride-sharing-app',
       'var-profileMetricId': 'process_cpu:cpu:nanoseconds:cpu:nanoseconds',
-      maxNodes: '16384',
+      // no max nodes here so that the app fetches user settings and we can also test settings changes
     })
   : new URLSearchParams();
 
-export const AUTH_FILE = path.join(process.cwd(), 'e2e', 'auth', 'user.json');
+export const EXPLORE_PROFILES_DIFF_RANGES_URL_PARAMS = new URLSearchParams({
+  // no max nodes here neither so that the app fetches user settings and we can also test settings changes
+  'from-2': '2024-03-13T18:00:00.000Z',
+  'to-2': '2024-03-13T18:50:00.000Z',
+  'from-3': '2024-03-13T18:00:00.000Z',
+  'to-3': '2024-03-13T18:50:00.000Z',
+  diffFrom: '2024-03-13T18:15:00.000Z',
+  diffTo: '2024-03-13T18:20:00.000Z',
+  'diffFrom-2': '2024-03-13T18:35:00.000Z',
+  'diffTo-2': '2024-03-13T18:45:00.000Z',
+});
--- e2e/tests/settings-view.spec.ts
+++ e2e/tests/settings-view.spec.ts
@@ -1,3 +1,4 @@
+import { ExplorationType, EXPLORE_PROFILES_DIFF_RANGES_URL_PARAMS } from '../config/constants';
 import { expect, test } from '../fixtures';
 
 test.beforeEach(async ({ settingsPage }) => {
@@ -5,30 +6,28 @@ test.beforeEach(async ({ settingsPage }) => {
 });
 
 test.afterEach(async ({ settingsPage }) => {
-  await settingsPage.resetTestSettings();
+  await settingsPage.resetTestSettings(false);
 });
 
 test.describe('Plugin Settings', () => {
   // prevents unwanted settings modifications while running multiple tests in parallel
   test.describe.configure({ mode: 'serial' });
 
-  test.describe('Smoke tests', () => {
-    test('Flame graph & Export settings', async ({ settingsPage }) => {
-      const flamegraphSettings = settingsPage.getFlamegraphSettings();
+  test('Main UI elements', async ({ settingsPage }) => {
+    const flamegraphSettings = settingsPage.getFlamegraphSettings();
 
-      await expect(flamegraphSettings).toBeVisible();
-      await expect(flamegraphSettings.getByText('Collapsed flame graphs')).toBeVisible();
-      await expect(flamegraphSettings.getByText('Maximum number of nodes')).toBeVisible();
+    await expect(flamegraphSettings).toBeVisible();
+    await expect(flamegraphSettings.getByText('Collapsed flame graphs')).toBeVisible();
+    await expect(flamegraphSettings.getByText('Maximum number of nodes')).toBeVisible();
 
-      const exportSettings = settingsPage.getExportSettings();
+    const exportSettings = settingsPage.getExportSettings();
 
-      await expect(exportSettings).toBeVisible();
-      await expect(exportSettings.getByText('Enable flamegraph.com')).toBeVisible();
-    });
+    await expect(exportSettings).toBeVisible();
+    await expect(exportSettings.getByText('Enable flamegraph.com')).toBeVisible();
   });
 
   test.describe('Flame graph settings', () => {
-    test('Can be modified', async ({ settingsPage, comparisonDiffViewPage }) => {
+    test('Can be modified for the "Flame graph" views', async ({ settingsPage, exploreProfilesPage }) => {
       await settingsPage.resetTestSettings();
 
       await settingsPage.getCollapsedFlamegraphsCheckbox().click();
@@ -36,22 +35,47 @@ test.describe('Plugin Settings', () => {
       await settingsPage.getSaveSettingsButton().click();
       await expect(settingsPage.getSuccessAlertDialog()).toBeVisible();
 
-      await comparisonDiffViewPage.goto();
-      await expect(comparisonDiffViewPage.getFlamegraph()).toHaveScreenshot();
+      // flame graph
+      await exploreProfilesPage.goto(ExplorationType.FlameGraph);
+
+      await exploreProfilesPage.clickOnFlameGraphNode({ x: 250, y: 50 });
+      await expect(exploreProfilesPage.getFlameGraphContextualMenuItem('Expand group')).toBeVisible();
+      await expect(exploreProfilesPage.getFlameGraphContextualMenuItem('Expand all groups')).toBeVisible();
+
+      await exploreProfilesPage.closeFlameGraphContextualMenu();
+      await expect(exploreProfilesPage.getFlamegraph()).toHaveScreenshot();
+
+      // diff flame graph
+      await exploreProfilesPage.goto(ExplorationType.DiffFlameGraph, EXPLORE_PROFILES_DIFF_RANGES_URL_PARAMS);
+
+      await exploreProfilesPage.clickOnFlameGraphNode({ x: 250, y: 50 });
+      await expect(exploreProfilesPage.getFlameGraphContextualMenuItem('Expand group')).toBeVisible();
+      await expect(exploreProfilesPage.getFlameGraphContextualMenuItem('Expand all groups')).toBeVisible();
+
+      await exploreProfilesPage.closeFlameGraphContextualMenu();
+      await expect(exploreProfilesPage.getFlamegraph()).toHaveScreenshot();
     });
   });
 
   test.describe('Export settings', () => {
-    test('Can be modified', async ({ settingsPage, comparisonDiffViewPage }) => {
+    test('Can be modified', async ({ settingsPage, exploreProfilesPage }) => {
       await settingsPage.resetTestSettings();
 
       await settingsPage.getEnableFlamegraphDotComCheckbox().click();
       await settingsPage.getSaveSettingsButton().click();
       await expect(settingsPage.getSuccessAlertDialog()).toBeVisible();
 
-      await comparisonDiffViewPage.goto();
-      await comparisonDiffViewPage.getExportDataButton().click();
-      await expect(comparisonDiffViewPage.getByText('flamegraph.com')).not.toBeVisible();
+      // flame graph
+      await exploreProfilesPage.goto(ExplorationType.FlameGraph);
+      await exploreProfilesPage.getExportDataButton().click();
+
+      await expect(exploreProfilesPage.getByText('flamegraph.com')).not.toBeVisible();
+
+      // diff flame graph
+      await exploreProfilesPage.goto(ExplorationType.DiffFlameGraph, EXPLORE_PROFILES_DIFF_RANGES_URL_PARAMS);
+      await exploreProfilesPage.getExportDataButton().click();
+
+      await expect(exploreProfilesPage.getByText('flamegraph.com')).not.toBeVisible();
     });
   });
 });
