1af470f480ca4065933f6254b387d896ba9b5e33
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-10-02 23:30:54 +0200

    chore: Remove unnecessary operators descriptions

--- src/shared/components/QueryBuilder/infrastructure/operatorsRepository.ts
+++ src/shared/components/QueryBuilder/infrastructure/operatorsRepository.ts
@@ -3,11 +3,11 @@ import { Suggestions } from '../domain/types';
 class OperatorsRepository {
   async list(): Promise<Suggestions> {
     return [
-      { value: '=', label: '=', description: 'Equals' },
-      { value: '!=', label: '!=', description: 'Not equal' },
+      { value: '=', label: '=' },
+      { value: '!=', label: '!=' },
+      { value: 'is-empty', label: 'is empty' },
       { value: 'in', label: 'in', description: 'Is one of' },
       { value: 'not-in', label: 'not in', description: 'Is not one of' },
-      { value: 'is-empty', label: 'is empty' },
       { value: '=~', label: '=~', description: 'Matches regex' },
       { value: '!~', label: '!~', description: 'Does not match regex' },
     ];
