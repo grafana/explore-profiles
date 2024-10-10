f40eaf3acf247b89bfd019c0b1a7c27334fbfcd5
Piotr Jamróz <pm.jamroz@gmail.com>
2024-10-10 14:07:40 +0200

    Add query link extensions

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,36 @@
 import { AppPlugin } from '@grafana/data';
+import { DataQuery, DataSourceRef } from '@grafana/schema';
 import { AppPluginSettings } from '@shared/types/AppPluginSettings';
 
 import { App } from './app/App';
 
-export const plugin = new AppPlugin<AppPluginSettings>().setRootPage(App);
+// TODO: replace with type from grafana/runtime
+const QueryToAppPlugin = 'grafana/query/query-to-app-plugin';
+
+// TODO: replace with type from grafana/runtime
+type QueryToAppPluginContext = {
+  datasource: DataSourceRef;
+  query: DataQuery;
+};
+
+export const plugin = new AppPlugin<AppPluginSettings>()
+  .addLink({
+    targets: [QueryToAppPlugin],
+    title: 'Explore with Profiles app',
+    description: 'Link to Explore Profiles app',
+    path: '/a/grafana-pyroscope-app/profiles-explorer',
+    configure(context: QueryToAppPluginContext | undefined) {
+      if (!context || !context.query) {
+        return undefined;
+      }
+
+      if (context.datasource.type !== 'grafana-pyroscope-datasource') {
+        return undefined;
+      }
+
+      return {
+        path: `/a/grafana-pyroscope-app/profiles-explorer?var-dataSource=${context.datasource.uid}`,
+      };
+    },
+  })
+  .setRootPage(App);
