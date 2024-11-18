0967f0ca6a5d0a716295c63dfe8fb348b85179d8
Piotr Jamróz <pm.jamroz@gmail.com>
2024-11-18 13:23:24 +0100

    Change extension point

--- src/module.ts
+++ src/module.ts
@@ -1,35 +1,76 @@
-import { AppPlugin } from '@grafana/data';
-import { DataQuery, DataSourceRef } from '@grafana/schema';
+import { AppPlugin, PluginExtensionPoints, RawTimeRange } from '@grafana/data';
+import { DataQuery } from '@grafana/schema';
+import { GrafanaPyroscopeDataQuery } from '@grafana/schema/dist/esm/raw/composable/grafanapyroscope/dataquery/x/GrafanaPyroscopeDataQuery_types.gen';
 import { AppPluginSettings } from '@shared/types/AppPluginSettings';
 
 import { App } from './app/App';
 
-// TODO: replace with type from grafana/runtime
-const QueryToAppPlugin = 'grafana/query/query-to-app-plugin';
-
-// TODO: replace with type from grafana/runtime
-type QueryToAppPluginContext = {
-  datasource: DataSourceRef;
-  query: DataQuery;
+type PluginExtensionExploreContext = {
+  targets: DataQuery[];
+  timeRange: RawTimeRange;
 };
 
+type URLParamsBuilderProps = {
+  pyroscopeQuery: GrafanaPyroscopeDataQuery;
+  timeRange?: RawTimeRange;
+};
+
+function buildURL(props: URLParamsBuilderProps) {
+  const { timeRange, pyroscopeQuery } = props;
+
+  let timeRangeParam = '';
+  let explorationType = 'all';
+
+  let serviceName = props.pyroscopeQuery.labelSelector?.match(/service_name="([^"]+)"/)?.[1];
+
+  if (serviceName) {
+    explorationType = 'labels';
+  }
+
+  const datasourceParam = `var-dataSource=${pyroscopeQuery.datasource?.uid}`;
+  const serviceNameParam = serviceName ? `&var-serviceName=${serviceName}` : '';
+  const profileTypeParam = `&var-profileMetricId=${pyroscopeQuery.profileTypeId}`;
+  const explorationTypeParam = `&explorationType=${explorationType}`;
+  if (timeRange) {
+    timeRangeParam = `&from=${timeRange.from}&to=${timeRange.to}`;
+  }
+
+  return `/a/grafana-pyroscope-app/profiles-explorer?${datasourceParam}${serviceNameParam}${profileTypeParam}${timeRangeParam}${explorationTypeParam}`;
+}
+
 export const plugin = new AppPlugin<AppPluginSettings>()
-  .addLink({
-    targets: [QueryToAppPlugin],
-    title: 'Explore Profiles',
+  .addLink<PluginExtensionExploreContext>({
+    targets: [PluginExtensionPoints.ExploreToolbarAction],
+    title: 'Open in Explore Profiles',
+    icon: 'fire',
     description: 'Try our new queryless experience for profiles',
     path: '/a/grafana-pyroscope-app/profiles-explorer',
-    configure(context: QueryToAppPluginContext | undefined) {
-      if (!context || !context.query || !context.datasource) {
+    configure(context: PluginExtensionExploreContext | undefined) {
+      if (!context || !context.targets || !context.timeRange) {
         return undefined;
       }
 
-      if (context.datasource.type !== 'grafana-pyroscope-datasource') {
+      if (context.targets.length > 1) {
         return undefined;
       }
+
+      const firstQuery = context.targets[0];
+
+      if (!firstQuery.datasource) {
+        return undefined;
+      }
+
+      if (firstQuery.datasource.type !== 'grafana-pyroscope-datasource') {
+        return undefined;
+      }
+
+      const path = buildURL({
+        pyroscopeQuery: firstQuery as GrafanaPyroscopeDataQuery,
+        timeRange: context.timeRange,
+      });
 
       return {
-        path: `/a/grafana-pyroscope-app/profiles-explorer?var-dataSource=${context.datasource.uid}`,
+        path: path,
       };
     },
   })
