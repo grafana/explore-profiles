8a5b668a1a8773fbcc424f48fbe7bedece1f85a9
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-12-11 20:13:20 +0100

    feat: Stricter validation

--- src/pages/ProfilesExplorerView/infrastructure/withPreventInvalidQuery.ts
+++ src/pages/ProfilesExplorerView/infrastructure/withPreventInvalidQuery.ts
@@ -1,8 +1,28 @@
 import { LoadingState } from '@grafana/data';
 import { sceneGraph, SceneQueryRunner } from '@grafana/scenes';
+import { parseQuery } from '@shared/domain/url-params/parseQuery';
+import { logger } from '@shared/infrastructure/tracking/logger';
 
 export function withPreventInvalidQuery(queryRunner: SceneQueryRunner) {
   queryRunner.addActivationHandler(() => {
+    const { profileTypeId, labelSelector } = queryRunner.state.queries[0];
+
+    if (!profileTypeId) {
+      queryRunner.setState({
+        queries: [{ refId: 'null' }],
+        data: buildErrorData(queryRunner, 'Missing profile type!'),
+      });
+      return;
+    }
+
+    if (!labelSelector) {
+      queryRunner.setState({
+        queries: [{ refId: 'null' }],
+        data: buildErrorData(queryRunner, 'Missing label selector!'),
+      });
+      return;
+    }
+
     if (!sceneGraph.interpolate(queryRunner, '$profileMetricId')) {
       queryRunner.setState({
         queries: [{ refId: 'null' }],
@@ -11,7 +31,9 @@ export function withPreventInvalidQuery(queryRunner: SceneQueryRunner) {
       return;
     }
 
-    if (!sceneGraph.interpolate(queryRunner, '$serviceName')) {
+    const parsed = parseQuery(sceneGraph.interpolate(queryRunner, `$profileTypeId${labelSelector})`));
+
+    if (!parsed.serviceId) {
       queryRunner.setState({
         queries: [{ refId: 'null' }],
         data: buildErrorData(queryRunner, 'Missing service name!'),
@@ -22,9 +44,15 @@ export function withPreventInvalidQuery(queryRunner: SceneQueryRunner) {
   return queryRunner;
 }
 
-const buildErrorData = (queryRunner: SceneQueryRunner, errorMsg: string) => ({
-  state: LoadingState.Error,
-  errors: [new Error(errorMsg)],
-  series: [],
-  timeRange: sceneGraph.getTimeRange(queryRunner).state.value,
-});
+function buildErrorData(queryRunner: SceneQueryRunner, errorMsg: string) {
+  const error = new Error(errorMsg);
+
+  logger.error(error);
+
+  return {
+    state: LoadingState.Error,
+    errors: [error],
+    series: [],
+    timeRange: sceneGraph.getTimeRange(queryRunner).state.value,
+  };
+}
