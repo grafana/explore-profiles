080ac02fd8cb788ec0e799c8bdea1763cb2440b1
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-12-18 11:38:19 +0100

    refactor(*): Lazy load page components

--- src/pages/ProfilesExplorerView/ProfilesExplorerView.tsx
+++ src/pages/ProfilesExplorerView/ProfilesExplorerView.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 
 import { SceneProfilesExplorer } from './components/SceneProfilesExplorer/SceneProfilesExplorer';
 
-export function ProfilesExplorerView() {
+export default function ProfilesExplorerView() {
   const sceneProfilesExplorer = useMemo(() => new SceneProfilesExplorer(), []);
 
   return <sceneProfilesExplorer.Component model={sceneProfilesExplorer} />;
