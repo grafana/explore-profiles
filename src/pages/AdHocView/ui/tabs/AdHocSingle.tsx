bb4c5205a599e6a1baddce7413cd986df287f34b
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-02-04 11:42:58 +0100

    chore: Track Ad Hoc view

--- src/pages/AdHocView/ui/tabs/AdHocSingle.tsx
+++ src/pages/AdHocView/ui/tabs/AdHocSingle.tsx
@@ -1,3 +1,5 @@
+import { SelectableValue } from '@grafana/data';
+import { reportInteraction } from '@shared/domain/reportInteraction';
 import React, { memo } from 'react';
 
 import { useUploadFile } from '../../domain/useUploadFile';
@@ -9,10 +11,25 @@ import { AdHocSpinner } from '../AdHocSpinner';
 function AdHocSingleComponent() {
   const { processFile, profileTypes, selectProfileType, profile, removeFile, isLoading } = useUploadFile();
 
+  const onChangeProfileType = (options: SelectableValue<string>) => {
+    reportInteraction('g_pyroscope_app_ad_hoc_profile_metric_selected');
+    selectProfileType(options);
+  };
+
+  const onFileDropped = (file: File) => {
+    reportInteraction('g_pyroscope_app_ad_hoc_file_dropped', { fileType: file.type });
+    processFile(file);
+  };
+
+  const onFileRemoved = () => {
+    reportInteraction('g_pyroscope_app_ad_hoc_file_removed');
+    removeFile();
+  };
+
   return (
     <div>
-      <AdHocProfileTypeSelector profileTypes={profileTypes} onChange={selectProfileType} />
-      <AdHocFileDropZone onFileDropped={processFile} onFileRemove={removeFile} />
+      <AdHocProfileTypeSelector profileTypes={profileTypes} onChange={onChangeProfileType} />
+      <AdHocFileDropZone onFileDropped={onFileDropped} onFileRemove={onFileRemoved} />
       {isLoading && !profile ? <AdHocSpinner /> : null}
       {profile && <AdHocFlameGraph profile={profile} />}
     </div>
