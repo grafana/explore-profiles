45bc71e27ac8306d735c980b3436b61889f522d9
Marc M. <146180665+grafakus@users.noreply.github.com>
2025-02-14 10:40:07 +0100

    fix(*): Remove lazy loading of page components (#422)

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { Routes as ReactRouterRoutes, Route } from 'react-router-dom';
 
 import { ROUTES } from '../constants';
-
-const ProfilesExplorerView = React.lazy(() => import('../pages/ProfilesExplorerView/ProfilesExplorerView'));
-const AdHocView = React.lazy(() => import('../pages/AdHocView/AdHocView'));
-const SettingsView = React.lazy(() => import('../pages/SettingsView/SettingsView'));
+import AdHocView from '../pages/AdHocView/AdHocView';
+import ProfilesExplorerView from '../pages/ProfilesExplorerView/ProfilesExplorerView';
+import SettingsView from '../pages/SettingsView/SettingsView';
 
 export function Routes() {
   return (
     <ReactRouterRoutes>
-      <Route path={`${ROUTES.EXPLORE}/*`} element={<ProfilesExplorerView />} />
-      <Route path={`${ROUTES.ADHOC}/*`} element={<AdHocView />} />
-      <Route path={`${ROUTES.SETTINGS}/*`} element={<SettingsView />} />
+      <Route path={ROUTES.EXPLORE} element={<ProfilesExplorerView />} />
+      <Route path={ROUTES.ADHOC} element={<AdHocView />} />
+      <Route path={ROUTES.SETTINGS} element={<SettingsView />} />
       {/* Default Route */}
       <Route path="/*" element={<ProfilesExplorerView />} />
     </ReactRouterRoutes>
