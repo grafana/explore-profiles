36514d9fd7863ee4537038c33145f73980e150d2
Piotr Jamróz <pm.jamroz@gmail.com>
2025-03-13 20:36:32 +0100

    Use InteractiveTable
    This way the table is more responsive, doesn't need data frame as data (Table component is more useful for rendering data frames)

--- src/pages/RecordingRulesView/RecordingRulesView.tsx
+++ src/pages/RecordingRulesView/RecordingRulesView.tsx
@@ -1,17 +1,5 @@
 import { css } from '@emotion/css';
-import { applyFieldOverrides, arrayToDataFrame, DataFrame, FieldType, GrafanaTheme2 } from '@grafana/data';
-import { TableCellHeight } from '@grafana/schema';
-import {
-  Button,
-  IconButton,
-  Table,
-  TableCellDisplayMode,
-  TableCustomCellOptions,
-  TagList,
-  Text,
-  useStyles2,
-  useTheme2,
-} from '@grafana/ui';
+import { Button, Column, DeleteButton, InteractiveTable, TagList, Text, useStyles2 } from '@grafana/ui';
 import { displayError } from '@shared/domain/displayStatus';
 import { getProfileMetric, ProfileMetricId } from '@shared/infrastructure/profile-metrics/getProfileMetric';
 import { RecordingRule } from '@shared/infrastructure/recording-rules/RecordingRule';
@@ -19,8 +7,6 @@ import { PageTitle } from '@shared/ui/PageTitle';
 import React from 'react';
 
 import { useRecordingRulesView } from './domain/useRecordingRulesView';
-
-type DeleteFn = (rule: RecordingRule) => Promise<void>;
 
 export default function RecordingRulesView() {
   const styles = useStyles2(getStyles);
@@ -34,30 +20,62 @@ export default function RecordingRulesView() {
     ]);
   }
 
-  // This is a bit of a hack to calculate the max width of the table since the
-  // Table api requires a fixed width value. Here we subtract a fixed amount
-  // from the window width to account for the sidebar.
-  const width = window.innerWidth - 400;
-  const theme = useTheme2();
-  const dataFrame =
-    recordingRules !== undefined
-      ? buildDataFrame(recordingRules, theme, width, styles, actions.removeRecordingRule)
-      : null;
+  const columns: Array<Column<RecordingRule>> = [
+    {
+      id: 'name',
+      header: 'Name',
+      sortType: 'alphanumeric',
+    },
+    {
+      id: 'serviceName',
+      header: 'Service Name',
+    },
+    {
+      id: 'profileType',
+      header: 'Profile Type',
+    },
+    {
+      id: 'labels',
+      header: 'Labels',
+      cell: (props) => {
+        // Exclude hidden labels.
+        const rule: RecordingRule = props.row.original;
+        const labels = rule.labels?.filter((label: string) => !label.match(/^__\S+__$/));
+
+        if (!labels || labels.length === 0) {
+          return (
+            <Text element="span" color="secondary">
+              None
+            </Text>
+          );
+        }
+
+        return <TagList className={styles.tagList} displayMax={4} tags={labels} />;
+      },
+    },
+    {
+      id: 'actions',
+      header: 'Actions',
+      disableGrow: true,
+      cell: (props) => {
+        const rule: RecordingRule = props.row.original;
+        return <DeleteButton onConfirm={() => actions.removeRecordingRule(rule)} />;
+      },
+    },
+  ];
+
+  const formattedRules = (recordingRules || []).map((rule) => {
+    const profileType = getProfileMetric(rule.profileType as ProfileMetricId);
+    return {
+      ...rule,
+      profileType: `${profileType.group}/${profileType.type}`,
+    };
+  });
 
   return (
     <>
       <PageTitle title="Recording rules" />
-      {dataFrame && (
-        <Table
-          data={dataFrame}
-          width={width}
-          height={500}
-          columnMinWidth={130}
-          cellHeight={TableCellHeight.Lg}
-          resizable={false}
-          initialSortBy={[{ displayName: 'Name' }]}
-        />
-      )}
+      <InteractiveTable columns={columns} data={formattedRules || []} getRowId={(rule) => rule.name}></InteractiveTable>
       <Button variant="secondary" onClick={() => history.back()} aria-label="Back to Profiles Drilldown">
         Back to Profiles Drilldown
       </Button>
@@ -71,121 +89,3 @@ const getStyles = () => ({
     justify-content: start;
   `,
 });
-
-function buildDataFrame(
-  rules: RecordingRule[],
-  theme: GrafanaTheme2,
-  tableWidth: number,
-  styles: Record<string, any>,
-  onDelete: DeleteFn
-): DataFrame {
-  const df = arrayToDataFrame(
-    rules.map((r) => {
-      const profileType = getProfileMetric(r.profileType as ProfileMetricId);
-
-      return {
-        Name: r.name,
-        'Service Name': r.serviceName,
-        'Profile Type': `${profileType.group}/${profileType.type}`,
-      };
-    })
-  );
-
-  const labelOptions: TableCustomCellOptions = {
-    type: TableCellDisplayMode.Custom,
-    cellComponent: (props) => {
-      // Exclude hidden labels.
-      const labels = rules[props.rowIndex]?.labels?.filter((label: string) => !label.match(/^__\S+__$/));
-
-      if (!labels || labels.length === 0) {
-        return (
-          <Text element="span" color="secondary">
-            None
-          </Text>
-        );
-      }
-
-      return <TagList className={styles.tagList} displayMax={4} tags={labels} />;
-    },
-  };
-
-  const actionOptions: TableCustomCellOptions = {
-    type: TableCellDisplayMode.Custom,
-    cellComponent: (props) => {
-      const name = props.frame.fields.find((f) => f.name === 'Name')?.values[props.rowIndex];
-      const label = name ? `Delete ${name}` : 'Delete recording rule';
-
-      // todo(bryan): Make this a confirmation modal.
-      return (
-        <IconButton
-          name="trash-alt"
-          variant="destructive"
-          aria-label={label}
-          tooltip={label}
-          onClick={() => onDelete(rules[props.rowIndex])}
-        />
-      );
-    },
-  };
-
-  df.fields = [
-    ...df.fields,
-    {
-      name: 'Labels',
-      type: FieldType.other,
-      values: [],
-      config: {
-        custom: {
-          cellOptions: labelOptions,
-        },
-      },
-    },
-    {
-      name: 'Actions',
-      type: FieldType.other,
-      values: [],
-      config: {
-        custom: {
-          cellOptions: actionOptions,
-          align: 'right',
-        },
-      },
-    },
-  ];
-
-  const colWidthPercentages = [
-    0.2, // Name
-    0.15, // Service name
-    0.2, // Profile type
-    0.3, // Labels
-    0.05, // Actions
-  ];
-
-  // Remap all the column widths.
-  df.fields = df.fields.map((f, i) => {
-    const width = tableWidth * colWidthPercentages[i];
-    return {
-      ...f,
-      config: {
-        ...f.config,
-        custom: {
-          ...f.config.custom,
-          width,
-        },
-      },
-    };
-  });
-
-  // note(bryanhuhta): This is necessary to get the table body to render
-  // properly. We don't actually use it to perform any overrides.
-  const [dataFrameWithOverrides] = applyFieldOverrides({
-    data: [df],
-    theme: theme,
-    replaceVariables: (value) => value,
-    fieldConfig: {
-      defaults: {},
-      overrides: [],
-    },
-  });
-  return dataFrameWithOverrides;
-}
