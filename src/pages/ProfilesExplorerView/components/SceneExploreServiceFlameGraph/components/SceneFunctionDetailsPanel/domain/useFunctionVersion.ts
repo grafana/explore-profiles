3008f67df81ee52154d088d0af6add021103d4a1
Piotr Jamróz <pm.jamroz@gmail.com>
2025-06-25 22:15:31 +0200

    Add missing file

---
+++ src/pages/ProfilesExplorerView/components/SceneExploreServiceFlameGraph/components/SceneFunctionDetailsPanel/domain/useFunctionVersion.ts
@@ -0,0 +1,45 @@
+import { useLocalStorage } from 'react-use';
+
+import { FunctionVersion } from './types/FunctionDetails';
+
+type OverridesStorage = Record<string, Record<string, FunctionVersion>>;
+
+export const useFunctionVersion = (
+  datasourceUid: string,
+  serviceName: string,
+  version: FunctionVersion | undefined
+) => {
+  const [overrides, setOverrides] = useLocalStorage<OverridesStorage>('functionDetailsOverrides', {});
+
+  return {
+    saveOverride: (datasourceUid: string, serviceName: string, version: FunctionVersion) => {
+      setOverrides((overrides) => {
+        if (!overrides) {
+          overrides = {};
+        }
+        if (!overrides[datasourceUid]) {
+          overrides[datasourceUid] = {};
+        }
+        overrides[datasourceUid][serviceName] = {
+          ...version,
+          custom: true,
+        };
+        return overrides;
+      });
+    },
+    deleteOverride: (datasourceUid: string, serviceName: string) => {
+      setOverrides((overrides) => {
+        if (!overrides || !overrides[datasourceUid] || !overrides[datasourceUid][serviceName]) {
+          return overrides;
+        }
+        delete overrides[datasourceUid][serviceName];
+        return overrides;
+      });
+    },
+    deleteAllOverrides: () => {
+      setOverrides({});
+    },
+    functionVersion: overrides?.[datasourceUid]?.[serviceName] || version,
+    // CODE: plus info about if override was applied
+  };
+};
