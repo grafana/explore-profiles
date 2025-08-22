f0ca705205ff3513ca2fb33a3aa47a5988b43319
MattIPv4 <me@mattcowley.co.uk>
2025-08-22 18:16:21 +0100

    Switch cloud-home-app plugin proxy usage to own

--- src/app/components/Onboarding/infrastructure/useFetchInstances.tsx
+++ src/app/components/Onboarding/infrastructure/useFetchInstances.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { PLUGIN_API_URL } from 'src/constants';
 
 export function useFetchInstances() {
   const { isFetching, error, data } = useQuery({
     queryKey: ['instances'],
     queryFn: () =>
-      fetch('/api/plugin-proxy/cloud-home-app/grafanacom-api/instances').then((response) => response.json()),
+      fetch(`${PLUGIN_API_URL}/grafanacom-api/instances`).then((response) => response.json()),
   });
 
   return {
