16531f608fa73c608e866993f43a09b698d41d51
Piotr Jamróz <pm.jamroz@gmail.com>
2025-10-08 16:58:40 +0200

    rename test id

--- src/pages/SettingsView/components/UISettingsView/UISettingsView.tsx
+++ src/pages/SettingsView/components/UISettingsView/UISettingsView.tsx
@@ -77,7 +77,7 @@ export function UISettingsView({ children }: { children: React.ReactNode }) {
       </FieldSet>
 
       {featureToggles.metricsFromProfiles && (
-        <FieldSet label="Metrics from profiles" data-testid="experimental-features">
+        <FieldSet label="Metrics from profiles" data-testid="metrics-from-profiles">
           <Alert severity="info" title="" className={css({ maxWidth: '1000px' })}>
             {data.enableMetricsFromProfiles ? (
               <>
