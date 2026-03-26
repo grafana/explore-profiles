354b4790207fb340312f6f893a0e034b954204c8
Miguel Palau Zarza <mpalauzarza@gmail.com>
2026-03-26 16:50:13 -0600

    fix: restore enableNewUI prop on SceneFlameGraph also removed during i18n

--- src/pages/ProfilesExplorerView/components/SceneExploreServiceFlameGraph/SceneFlameGraph.tsx
+++ src/pages/ProfilesExplorerView/components/SceneExploreServiceFlameGraph/SceneFlameGraph.tsx
@@ -288,6 +288,7 @@ export class SceneFlameGraph extends SceneObjectBase<SceneFlameGraphState> {
                 />
               }
               keepFocusOnDataChange
+              enableNewUI={featureToggles.flameGraphWithCallTree}
             />
           )}
         </Panel>
