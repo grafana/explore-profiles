00f7afb1e9f81cd812ca5987dc52e1a0d279ab5d
Joey <joey.tawadrous@grafana.com>
2026-04-02 10:34:44 +0100

    Fix tests

--- jest-setup.js
+++ jest-setup.js
@@ -3,3 +3,17 @@ import './.config/jest-setup';
 
 // IntersectionObserver is not available in jsdom (used by @grafana/scenes LazyLoader)
 global.IntersectionObserver = class IntersectionObserver {};
+
+// Emotion + :has() can produce selectors nwsapi cannot parse; Floating UI's Modal focus trap
+// calls getComputedStyle and throws. Fall back so tab order / visibility checks still run.
+const origGetComputedStyle = window.getComputedStyle.bind(window);
+window.getComputedStyle = (element, pseudoElt) => {
+  try {
+    return origGetComputedStyle(element, pseudoElt);
+  } catch {
+    return {
+      getPropertyValue: () => '',
+      visibility: 'visible',
+    };
+  }
+};
