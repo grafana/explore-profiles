2a501ff372c091337868b3470f40d7bfac07f41e
Joey <joey.tawadrous@grafana.com>
2026-05-29 07:40:03 +0100

    Grafana PR title validation

---
+++ .github/commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'I18n',
+      ],
+    ],
+    // Preserve legacy `I18n` type (not lowercase).
+    'type-case': [0],
+  },
+};
