1e73e9f633771c694fbb76c0a4335d0ea50d552a
Joey <joey.tawadrous@grafana.com>
2026-06-16 11:02:34 +0100

    Auth

--- src/exposedComponents/EmbeddedProfilesExploration/EmbeddedProfilesExploration.tsx
+++ src/exposedComponents/EmbeddedProfilesExploration/EmbeddedProfilesExploration.tsx
@@ -26,7 +26,7 @@ function buildProfilesExplorationFromState({
 
   const exploration = new SceneProfilesExplorer({
     $timeRange,
-    isEmbedded: true,
+    isEmbedded: false,
     initialFilters: initialFilters,
     initialDS,
   });
